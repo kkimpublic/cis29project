67eeb6fea1e80fa0d220ef0fd598cdc3d374ae81
Maladay <33292085+Maladay@users.noreply.github.com>
2020-01-30 12:12:43 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
   std::cout <<"Hello World!" <<std::endl;
+  std::string x;
+  std::cout << "Input for string " <<std::endl;
+  std::cin >> x;
+  std::cout << x;
   return 0;
 }
